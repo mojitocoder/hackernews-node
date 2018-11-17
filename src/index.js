31cb0f7d80e12b512f827266059c23f31a6cc2db
Quynh Nguyen <quynhnh@gmail.com>
2018-11-17 22:53:19 +0000

    Add post mutation

--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// Define GraphQL schema
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
-
 let links = [{
   id: 'link-0',
   url: 'www.simplybusiness.co.uk',
   description: 'Simply Business website'
 }]
 
+let idCount = links.length
+
 const resolvers = {
   Query: {
     info: () => `Insurance for small businesses`,
     feed: () => links,
   },
-  // Link: {
-  //   id: (root) => root.id,
-  //   description: (root) => root.description,
-  //   url: (root) => root.url,
-  // }
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql' ,
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
