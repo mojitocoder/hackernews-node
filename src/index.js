6a6a64935364512aa92c4319c91bff1fc5f29bee
Quynh Nguyen <quynhnh@gmail.com>
2018-11-17 21:54:26 +0000

    Update schema to add Link type

--- src/index.js
+++ src/index.js
@@ -4,12 +4,31 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
+
+let links = [{
+  id: 'link-0',
+  url: 'www.simplybusiness.co.uk',
+  description: 'Simply Business website'
+}]
+
 const resolvers = {
   Query: {
-    info: () => `This is the only field for now`
-    // info: () => null,
+    info: () => `Insurance for small businesses`,
+    feed: () => links,
+  },
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url,
   }
 }
 
